c1b35432a14b0893957e513079d4c91d9e9b9361
mazid70 <mazidurmazid431@gmail.com>
2026-01-09 17:53:14 +0600

    refresh token removed

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,29 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { createContext } from 'react';
-import axiosData from '../customHooks/useCallData'; // আপনার axios instance
+import useCallData from '../customHooks/useCallData';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const fetchUser = async () => {
-    try {
-      // প্রথমে access_token দিয়ে try
-      const res = await axiosData.get('/users/me');
-      return res.data.user;
-    } catch (err) {
-      // যদি 401 আসে → refresh token দিয়ে auto retry
-      if (err.response?.status === 401) {
-        try {
-          await axiosData.post('/users/refresh-token'); // refresh token route
-          const resRetry = await axiosData.get('/users/me'); // retry
-          return resRetry.data.user;
-        } catch (refreshErr) {
-          throw refreshErr; // refresh token failed → user must login again
-        }
-      }
-      throw err; // অন্য error
-    }
-  };
+  const axiosData = useCallData();
 
   const {
     data: user = null,
@@ -31,15 +13,18 @@ const AuthProvider = ({ children }) => {
     isLoading,
   } = useQuery({
     queryKey: ['user'],
-    queryFn: fetchUser,
+    queryFn: async () => {
+      const res = await axiosData.get('/users/me');
+      return res.data.user;
+    },
     retry: false,
     staleTime: 0,
   });
-console.log(user)
+
   const handleLogout = async () => {
     try {
       await axiosData.post('/users/logout');
-      window.location.reload();
+      window.location.reload()
     } catch (err) {
       console.log(err);
     }
